03b85c3c229e9802fee1de2037e1bb13727c562a
Brandon Edwards <edwardsb@uoguelph.ca>
2021-01-13 23:48:58 -0500

    distance model filter only landbirds

--- src/3-QPAD-distance-model.R
+++ src/3-QPAD-distance-model.R
@@ -3,7 +3,7 @@
 # NA-POPS: analysis
 # 3-QPAD-distance-model.R
 # Created August 2020
-# Last Updated December 2020
+# Last Updated January 2021
 
 ####### Import Libraries and External Files #######
 
@@ -17,12 +17,17 @@ library(foreach)
 load(file = here::here("data/dist_count_matrix.rda"))
 load(file = here::here("data/landcover_covariates.rda"))
 load(file = here::here("data/dist_design.rda"))
+na_sp_list <- read.csv("../utilities/IBP-Alpha-Codes20.csv")
 
 ####### Data Wrangling ############################
 
 # Drop most of the landcover covariates for now
 covariates_reduced <- landcover_covariates[, c("Sample_ID", "roaddist", "ForestOnly_5x5")]
 covariates_reduced$ForestOnly_5x5 <- covariates_reduced$ForestOnly_5x5 / 25
+
+# Drop invalid road distances, and create roadside variable
+covariates_reduced <- covariates_reduced[which(covariates_reduced$roaddist > 0), ]
+covariates_reduced$roadside <- ifelse(covariates_reduced$roaddist < 30, 1, 0)
 
 # Drop method I
 dist_count_matrix <- dist_count_matrix[-which(dist_count_matrix$Distance_Method == "I"), ]
@@ -65,7 +70,7 @@ for (i in col_names)
 covars <- plyr::join(counts, covariates_reduced, by = "Sample_ID", type = "left", match = "all")
 
 # Remove rows with NA covariates
-to_remove <- which(is.na(covars$roaddist))
+to_remove <- which(is.na(covars$roadside))
 counts <- counts[-c(to_remove), ]
 design <- design[-c(to_remove), ]
 covars <- covars[-c(to_remove), ]
@@ -75,8 +80,10 @@ counts <- counts[-c(to_remove), ]
 design <- design[-c(to_remove), ]
 covars <- covars[-c(to_remove), ]
 
-# Build matrices by species :D
-species <- sort(as.character(unique(counts$Species)))
+# Build matrices by species
+species_all <- sort(as.character(unique(counts$Species)))
+landbirds <- na_sp_list[which(na_sp_list$LANDBIRD == "TRUE"), "SPEC"]
+species <- species_all[which(species_all %in% landbirds)]
 
 for (s in species)
 {
@@ -109,10 +116,10 @@ foreach(sp = names(input_list), .packages = 'detect') %dopar%
   {
     x <- input_list[[sp]]
     m1 = cmulti(x$Y | x$D ~ 1, type="dis")
-    m2 = cmulti(x$Y | x$D ~ x$C$roaddist, type="dis")
+    m2 = cmulti(x$Y | x$D ~ x$C$roadside, type="dis")
     m3 = cmulti(x$Y | x$D ~ x$C$ForestOnly_5x5, type="dis")
-    m4 = cmulti(x$Y | x$D ~ x$C$roaddist + x$C$ForestOnly_5x5, type="dis")
-    m5 = cmulti(x$Y | x$D ~ x$C$roaddist * x$C$ForestOnly_5x5, type="dis")
+    m4 = cmulti(x$Y | x$D ~ x$C$roadside + x$C$ForestOnly_5x5, type="dis")
+    m5 = cmulti(x$Y | x$D ~ x$C$roadside * x$C$ForestOnly_5x5, type="dis")
     distance_list <- list(m1, m2, m3, m4, m5)
     save(distance_list, file = paste0("data/distance/", sp, ".rda"))    
   }
