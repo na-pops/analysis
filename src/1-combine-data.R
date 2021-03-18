e449b9dffe64601fa380b717deb5579307bb46f5
Brandon Edwards <brandon.edwards144@gmail.com>
2021-03-18 09:21:52 -0400

    added command line feedback to ease worrying while running

--- src/1-combine-data.R
+++ src/1-combine-data.R
@@ -3,7 +3,7 @@
 # NA-POPS: analysis
 # 1-combine-data.R
 # Created August 2020
-# Last Updated January 2021
+# Last Updated March 2021
 
 ####### Import Libraries and External Files #######
 
@@ -21,6 +21,7 @@ n_proj <- nrow(project_list)
 # Create combined count data frame
 project_counts <- vector('list', n_proj)
 project_samples <- vector('list', n_proj)
+message("1/7 Now reading and combining project counts and samples.\n")
 for (i in 1:n_proj)
 {
   p <- project_list$V1[i]
@@ -46,6 +47,7 @@ project_samples <- do.call(rbind, project_samples)
 ####### Wrangle Data ##############################
 
 # Create time count matrix
+message("2/7 Now creating time_count_matrix with dcast().\n")
 time_only <- project_counts[, c(1:3, 7,8 )]
 time_only <- time_only[-which(time_only$Time_Method == "ZZ"), ]
 time_only <- time_only[-which(is.na(time_only$Time_Level)), ]
@@ -55,6 +57,7 @@ time_count_matrix <- dcast(time_only,
                            fun.aggregate = sum)
 
 # Create distance count matrix
+message("3/7 Now creating dist_count_matrix with dcast().\n")
 dist_only <- project_counts[, c(1:5)]
 dist_only <- dist_only[-which(dist_only$Distance_Method == "ZZ"), ]
 dist_only <- dist_only[-which(is.na(dist_only$Distance_Level)), ]
@@ -66,7 +69,7 @@ dist_count_matrix <- dcast(dist_only,
 # Create combined landcover covariate df and temporal covariate df
 landcover_covariates <- vector('list', n_proj)
 temporal_covariates <- vector('list', n_proj)
-
+message("4/7 Now reading and combining covariates.\n")
 for (i in 1:n_proj)
 {
   p <- project_list$V1[i]
@@ -87,17 +90,19 @@ landcover_covariates <- do.call(rbind, landcover_covariates)
 temporal_covariates <- do.call(rbind, temporal_covariates)
 
 # Create time removal design matrix
+message("5/7 Now creating time_design with dcast().\n")
 time <- read.csv("../covariates/survey/time_lookup.csv")
 time <- time[-c(1), ]
 time_design <- dcast(time, Method + Max_Duration ~ Level, value.var = "End_Duration")
 
 # Create distance design matrix
+message("6/7 Now creating dist_design with dcast().\n")
 dist <- read.csv("../covariates/survey/distance_lookup.csv")
 dist <- dist[-c(1), ]
 dist_design <- dcast(dist, Method + Max_Distance ~ Level, value.var = "End_Distance")
 
 ####### Output Data ###############################
-
+message("7/7 Now saving all data.\n")
 save(project_counts, file = here::here("data/counts.rda"))
 save(project_samples, file = here::here("data/samples.rda"))
 save(time_count_matrix, file = here::here("data/time_count_matrix.rda"))
