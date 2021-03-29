5becff94f8df8c1d5cc917ef46157e3d2d9911e1
Brandon Edwards <brandon.edwards144@gmail.com>
2021-03-29 08:31:51 -0400

    changing order of processes

--- src/1-combine-data.R
+++ src/1-combine-data.R
@@ -13,6 +13,9 @@ source("../utilities/get-data.R")
 
 ####### Read Data #################################
 
+time <- read.csv("../covariates/survey/time_lookup.csv")
+dist <- read.csv("../covariates/survey/distance_lookup.csv")
+
 # Get project names
 project_list <- read.table("../utilities/proj-list")
 n_proj <- nrow(project_list)
@@ -43,32 +46,10 @@ for (i in 1:n_proj)
 project_counts <- do.call(rbind, project_counts)
 project_samples <- do.call(rbind, project_samples)
 
-####### Wrangle Data ##############################
-
-# Create time count matrix
-message("2/7 Now creating time_count_matrix with dcast().\n")
-time_only <- project_counts[, c(1:3, 7,8 )]
-time_only <- time_only[-which(time_only$Time_Method == "ZZ"), ]
-time_only <- time_only[-which(is.na(time_only$Time_Level)), ]
-time_count_matrix <- dcast(time_only,
-                           Sample_ID + Species + Time_Method ~ as.numeric(Time_Level),
-                           value.var = "Abundance",
-                           fun.aggregate = sum)
-
-# Create distance count matrix
-message("3/7 Now creating dist_count_matrix with dcast().\n")
-dist_only <- project_counts[, c(1:5)]
-dist_only <- dist_only[-which(dist_only$Distance_Method == "ZZ"), ]
-dist_only <- dist_only[-which(is.na(dist_only$Distance_Level)), ]
-dist_count_matrix <- dcast(dist_only,
-                           Sample_ID + Species + Distance_Method ~ as.numeric(Distance_Level),
-                           value.var = "Abundance",
-                           fun.aggregate = sum)
-
 # Create combined landcover covariate df and temporal covariate df
 landcover_covariates <- vector('list', n_proj)
 temporal_covariates <- vector('list', n_proj)
-message("4/7 Now reading and combining covariates.\n")
+message("2/7 Now reading and combining covariates.\n")
 for (i in 1:n_proj)
 {
   p <- project_list$V1[i]
@@ -88,15 +69,35 @@ for (i in 1:n_proj)
 landcover_covariates <- do.call(rbind, landcover_covariates)
 temporal_covariates <- do.call(rbind, temporal_covariates)
 
+####### Wrangle Data ##############################
+
+# Create time count matrix
+message("3/7 Now creating time_count_matrix with dcast().\n")
+time_only <- project_counts[, c(1:3, 7,8 )]
+time_only <- time_only[-which(time_only$Time_Method == "ZZ"), ]
+time_only <- time_only[-which(is.na(time_only$Time_Level)), ]
+time_count_matrix <- dcast(time_only,
+                           Sample_ID + Species + Time_Method ~ as.numeric(Time_Level),
+                           value.var = "Abundance",
+                           fun.aggregate = sum)
+
+# Create distance count matrix
+message("4/7 Now creating dist_count_matrix with dcast().\n")
+dist_only <- project_counts[, c(1:5)]
+dist_only <- dist_only[-which(dist_only$Distance_Method == "ZZ"), ]
+dist_only <- dist_only[-which(is.na(dist_only$Distance_Level)), ]
+dist_count_matrix <- dcast(dist_only,
+                           Sample_ID + Species + Distance_Method ~ as.numeric(Distance_Level),
+                           value.var = "Abundance",
+                           fun.aggregate = sum)
+
 # Create time removal design matrix
 message("5/7 Now creating time_design with dcast().\n")
-time <- read.csv("../covariates/survey/time_lookup.csv")
 time <- time[-c(1), ]
 time_design <- dcast(time, Method + Max_Duration ~ Level, value.var = "End_Duration")
 
 # Create distance design matrix
 message("6/7 Now creating dist_design with dcast().\n")
-dist <- read.csv("../covariates/survey/distance_lookup.csv")
 dist <- dist[-c(1), ]
 dist_design <- dcast(dist, Method + Max_Distance ~ Level, value.var = "End_Distance")
 
