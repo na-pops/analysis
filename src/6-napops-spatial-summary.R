624bf8e422316fe7c3b21479697990aa9644374f
Brandon Edwards <brandon.edwards144@gmail.com>
2021-12-17 14:27:13 -0500

    changes to how data are saved, much more efficient this way

--- src/6-napops-spatial-summary.R
+++ src/6-napops-spatial-summary.R
@@ -3,16 +3,17 @@
 # NA-POPS: analysis
 # 6-napops-spatial-summary.R
 # Created February 2021
-# Last Updated November 2021
+# Last Updated December 2021
 
 ####### Import Libraries and External Files #######
 
 library(sf)
-library(tidyverse)
+library(magrittr)
 
 ####### Set Constants #############################
 
 laea <- 4326# sf::st_crs("+proj=laea +lat_0=40 +lon_0=-95") # Lambert equal area coord reference system
+sf::sf_use_s2(FALSE)
 
 ####### Read Data #################################
 
@@ -29,46 +30,6 @@ load(file = "data/combined/rem_covars_used.rda")
 
 bcr <- read_sf("../utilities/shp/bcr",
                  layer = "BBS_BCR_strata")
-strat <- read_sf("../utilities/shp/bcr_state",
-                    layer = "BBS_usgs_strata")
-state <- read_sf("../utilities/shp/state",
-                    layer = "BBS_ProvState_strata")
-
-####### Analysis by BCR x State/Prov ##############
-
-coords <- project_samples[, c("Latitude", "Longitude")]
-
-coords <- coords[!is.na(coords$Latitude), ]
-coords <- coords[!is.na(coords$Longitude), ]
-
-coords <- st_as_sf(coords,coords = c("Longitude","Latitude"), crs = laea)
-strat <- strat %>% st_transform(st_crs(coords))
-
-counts_strat <- st_intersects(strat, coords)
-bcr_state_coverage <- data.frame(ST_12 = strat$ST_12,
-                             ncounts = lengths(counts_strat),
-                             sqrt_ncounts = sqrt(lengths(counts_strat)),
-                             stringsAsFactors = FALSE)
-bcr_state_coverage$nc_cat <- cut(bcr_state_coverage$ncounts, breaks = c(-1,0,(c(100,200,500,1000,15000))))
-bcr_state_coverage <- left_join(strat, bcr_state_coverage)
-
-####### Analysis by State Only ####################
-
-coords <- project_samples[, c("Latitude", "Longitude")]
-
-coords <- coords[!is.na(coords$Latitude), ]
-coords <- coords[!is.na(coords$Longitude), ]
-
-coords <- st_as_sf(coords,coords = c("Longitude","Latitude"), crs = laea)
-state <- state %>% st_transform(st_crs(coords))
-
-counts_state <- st_intersects(state, coords)
-state_coverage <- data.frame(ST_12 = state$ST_12,
-                             ncounts = lengths(counts_state),
-                             sqrt_ncounts = sqrt(lengths(counts_state)),
-                             stringsAsFactors = FALSE)
-state_coverage$nc_cat <- cut(state_coverage$ncounts, breaks = c(-1,0,(c(100,200,500,1000,15000))))
-state_coverage <- left_join(state, state_coverage)
 
 ####### Analysis by BCR ####################
 
@@ -81,12 +42,11 @@ coords <- st_as_sf(coords,coords = c("Longitude","Latitude"), crs = laea)
 bcr <- bcr %>% st_transform(st_crs(coords))
 
 counts_bcr <- st_intersects(bcr, coords)
-bcr_coverage <- data.frame(ST_12 = bcr$ST_12,
+bcr_coverage <- data.frame(BCR = bcr$ST_12,
                            ncounts = lengths(counts_bcr),
                            sqrt_ncounts = sqrt(lengths(counts_bcr)),
                            stringsAsFactors = FALSE)
 bcr_coverage$nc_cat <- cut(bcr_coverage$ncounts, breaks = c(-1,0,(c(100,200,500,1000,15000))))
-bcr_coverage <- left_join(bcr, bcr_coverage)
 
 ####### Analysis by Species (BCR, Distance) #######
 
@@ -109,12 +69,11 @@ for (sp in names(dis_species_summary))
   bcr <- bcr %>% st_transform(st_crs(coords))
   
   counts_bcr_sp <- st_intersects(bcr, coords)
-  bcr_coverage_sp <- data.frame(ST_12 = bcr$ST_12,
+  bcr_coverage_sp <- data.frame(BCR = bcr$ST_12,
                              ncounts = lengths(counts_bcr_sp),
                              sqrt_ncounts = sqrt(lengths(counts_bcr_sp)),
                              stringsAsFactors = FALSE)
   bcr_coverage_sp$nc_cat <- cut(bcr_coverage_sp$ncounts, breaks = c(-1,0,(c(100,200,500,1000,15000))))
-  bcr_coverage_sp <- left_join(bcr, bcr_coverage_sp)
   bcr_dis_coverage[[sp]] <- bcr_coverage_sp
 }
 
@@ -139,19 +98,16 @@ for (sp in names(rem_species_summary))
   bcr <- bcr %>% st_transform(st_crs(coords))
   
   counts_bcr_sp <- st_intersects(bcr, coords)
-  bcr_coverage_sp <- data.frame(ST_12 = bcr$ST_12,
+  bcr_coverage_sp <- data.frame(BCR = bcr$ST_12,
                                 ncounts = lengths(counts_bcr_sp),
                                 sqrt_ncounts = sqrt(lengths(counts_bcr_sp)),
                                 stringsAsFactors = FALSE)
   bcr_coverage_sp$nc_cat <- cut(bcr_coverage_sp$ncounts, breaks = c(-1,0,(c(100,200,500,1000,15000))))
-  bcr_coverage_sp <- left_join(bcr, bcr_coverage_sp)
   bcr_rem_coverage[[sp]] <- bcr_coverage_sp
 }
 
 ####### Output Summary Statistics and Tables ######
 
-save(bcr_state_coverage, file = "../results/spatial-summary/project_coverage_bcr_state.rda")
-save(state_coverage, file = "../results/spatial-summary/project_coverage_state.rda")
 save(bcr_coverage, file = "../results/spatial-summary/project_coverage_bcr.rda")
 save(bcr_dis_coverage, file = "../results/spatial-summary/dis_coverage_bcr.rda")
 save(bcr_rem_coverage, file = "../results/spatial-summary/rem_coverage_bcr.rda")
