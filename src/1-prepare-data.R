55562cb033a244ee29e78b18c7512fbbd399b0d7
Brandon Edwards <brandon.edwards144@gmail.com>
2023-02-19 10:54:41 -0500

    move all data wrangling code into prepare-data script. including adding NAs to the count array

--- src/1-prepare-data.R
+++ src/1-prepare-data.R
@@ -24,7 +24,7 @@ n_proj <- nrow(project_list)
 # Create combined count data frame
 project_counts <- vector('list', n_proj)
 project_samples <- vector('list', n_proj)
-message("1/6 Now reading and combining project counts and samples.\n")
+message("1/5 Now reading and combining project counts and samples.\n")
 for (i in 1:n_proj)
 {
   p <- project_list$V1[i]
@@ -62,7 +62,7 @@ project_samples <- project_samples[which(project_samples$Sample_ID %in% project_
 # Create combined landcover covariate df and temporal covariate df
 landcover_covariates <- vector('list', n_proj)
 temporal_covariates <- vector('list', n_proj)
-message("2/6 Now reading and combining covariates.\n")
+message("2/5 Now reading and combining landcover and temporal covariates.\n")
 for (i in 1:n_proj)
 {
   p <- project_list$V1[i]
@@ -85,8 +85,8 @@ temporal_covariates <- do.call(rbind, temporal_covariates)
 ####### Wrangle Data ##############################
 
 # Create joint time and distance matrix
-message("3/6 Now creating joint time and distance count matrix with dcast().\n")
-joint_matrix <- project_counts
+message("3/5 Now creating joint distance and time count matrix with dcast().\n")
+joint_matrix <- project_counts[c(1:3000), ]
 joint_matrix <- joint_matrix[-which(joint_matrix$Time_Method == "ZZ"), ]
 joint_matrix <- joint_matrix[-which(is.na(joint_matrix$Time_Level)), ]
 joint_matrix <- joint_matrix[-which(joint_matrix$Time_Level == "99"), ]
@@ -94,42 +94,62 @@ joint_matrix <- joint_matrix[-which(joint_matrix$Distance_Method == "ZZ"), ]
 joint_matrix <- joint_matrix[-which(is.na(joint_matrix$Distance_Level)), ]
 
 joint_count_matrix <- dcast(joint_matrix,
-                            Sample_ID + Species + Time_Method + Distance_Method +
-                              Distance_Level ~ as.numeric(Time_Level),
+                            Sample_ID + Species + Distance_Method + Time_Method +
+                              Time_Level ~ as.numeric(Distance_Level),
                             value.var = "Abundance",
                             fun.aggregate = sum)
-joint_count_array <- array(data = 0, dim = c(nrow(joint_count_matrix),
+joint_count_metadata <- joint_count_matrix[, c("Sample_ID", "Species", "Time_Method",
+                                               "Distance_Method")]
+# Add indexing variable
+joint_count_metadata$Index <- seq(1,nrow(joint_count_metadata))
+
+# Create time design matrix
+time <- time[-c(1), ]
+time_design <- dcast(time, Method + Max_Duration ~ Level, value.var = "End_Duration")
+names(time_design)[1] <- "Time_Method"
+time_count_design <- plyr::join(joint_count_metadata[,c("Index", "Time_Method")], time_design,
+                                by = "Time_Method", type = "left")
+dist <- dist[-c(1), ]
+dist_design <- dcast(dist, Method + Max_Distance ~ Level, value.var = "End_Distance")
+names(dist_design)[1] <- "Distance_Method"
+dist_count_design <- plyr::join(joint_count_metadata[,c("Index", "Distance_Method")], dist_design,
+                                by = "Distance_Method", type = "left")
+
+message("4/5 Now populating joint count array with counts and NAs (this takes a while).\n")
+# Change 0s to NA in counts table where appropriate based on design table
+for (i in 6:ncol(joint_count_matrix))
+{
+  indices <- which(joint_count_matrix[, i] == 0)
+  
+  joint_count_matrix[indices, i] <- ifelse(is.na(dist_count_design[indices, i-2]), NA, 0)
+}
+
+joint_count_array <- array(data = NA, dim = c(nrow(joint_count_matrix),
                                              ncol(joint_count_matrix) - 5,
-                                             max(as.numeric(joint_count_matrix$Distance_Level))))
+                                             max(as.numeric(joint_count_matrix$Time_Level))))
 for (s in 1:(nrow(joint_count_matrix)))
 {
-  joint_count_array[s, , as.numeric(joint_count_matrix[s,]$Distance_Level)] <-
+  bound <- length(which(!is.na(joint_count_matrix[s, c(6:ncol(joint_count_matrix))])))
+  for (i in 1:bound)
+  {
+    joint_count_array[s,i,] <- ifelse(is.na(time_count_design[s,4:ncol(time_count_design)]), NA, 0)
+  }
+  joint_count_array[s,
+                     ,
+                    as.numeric(joint_count_matrix[s,]$Time_Level)] <-
     as.numeric(unname(joint_count_matrix[s, c(6:ncol(joint_count_matrix))]))
 }
 
-joint_count_metadata <- joint_count_matrix[, c("Sample_ID", "Species", "Time_Method",
-                                               "Distance_Method")]
-
-# Create time removal design matrix
-message("4/6 Now creating time_design with dcast().\n")
-time <- time[-c(1), ]
-time_design <- dcast(time, Method + Max_Duration ~ Level, value.var = "End_Duration")
-
-# Create distance design matrix
-message("5/6 Now creating dist_design with dcast().\n")
-dist <- dist[-c(1), ]
-dist_design <- dcast(dist, Method + Max_Distance ~ Level, value.var = "End_Distance")
-
 ####### Output Data ###############################
-message("6/6 Now saving all data.\n")
+message("5/5 Now saving all data.\n")
 save(project_counts, file = "data/combined/counts.rda")
 save(project_samples, file = "data/combined/samples.rda")
 save(joint_count_array, file = "data/combined/joint_count_array.rda")
 save(joint_count_metadata, file = "data/combined/joint_count_metadata.rda")
 save(landcover_covariates, file = "data/combined/landcover_covariates.rda")
 save(temporal_covariates, file = "data/combined/temporal_covariates.rda")
-save(time_design, file = "data/combined/time_design.rda")
-save(dist_design, file = "data/combined/dist_design.rda")
+save(time_count_design, file = "data/combined/time_count_design.rda")
+save(dist_count_design, file = "data/combined/dist_count_design.rda")
 date_file <- file("../results/date.txt")
 writeLines(as.character(Sys.Date()), date_file)
 close(date_file)
